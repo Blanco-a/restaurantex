2fc4fcc2ff5ca2639ac19a05ba75498bdb428d8b
Blanco-a <2064576@talnet.nl>
2021-11-09 09:34:50 +0100

    first c

---
+++ SQL/restaurantex.sql
@@ -0,0 +1,231 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2021 at 09:13 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restaurantex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bestellingen`
+--
+
+CREATE TABLE `bestellingen` (
+  `ID` int(11) NOT NULL,
+  `Reservering_ID` int(11) NOT NULL,
+  `Menuitem_ID` int(11) NOT NULL,
+  `Aantal` int(11) NOT NULL,
+  `Geserveerd` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gerechtcategorien`
+--
+
+CREATE TABLE `gerechtcategorien` (
+  `ID` int(11) NOT NULL,
+  `Code` varchar(3) NOT NULL,
+  `Naam` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gerechtsoorten`
+--
+
+CREATE TABLE `gerechtsoorten` (
+  `ID` int(11) NOT NULL,
+  `Code` varchar(3) NOT NULL,
+  `Naam` varchar(20) NOT NULL,
+  `Gerechtcategorie_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klanten`
+--
+
+CREATE TABLE `klanten` (
+  `ID` int(11) NOT NULL,
+  `Naam` varchar(20) NOT NULL,
+  `Telefoon` varchar(11) NOT NULL,
+  `Email` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menuitems`
+--
+
+CREATE TABLE `menuitems` (
+  `ID` int(11) NOT NULL,
+  `Code` varchar(4) NOT NULL,
+  `Naam` varchar(30) NOT NULL,
+  `Gerechtsoort_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reserveringen`
+--
+
+CREATE TABLE `reserveringen` (
+  `ID` int(11) NOT NULL,
+  `Tafel` int(11) NOT NULL,
+  `Datum` date NOT NULL,
+  `Tijd` time NOT NULL,
+  `Klant_ID` int(11) NOT NULL,
+  `Aantal` int(11) NOT NULL,
+  `Status` tinyint(4) NOT NULL,
+  `Datum_toegevoegd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Aantal_k` int(11) NOT NULL,
+  `Allergieen` text NOT NULL,
+  `Opmerkingen` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bestellingen`
+--
+ALTER TABLE `bestellingen`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Reservering_ID` (`Reservering_ID`),
+  ADD KEY `Menuitem_ID` (`Menuitem_ID`);
+
+--
+-- Indexes for table `gerechtcategorien`
+--
+ALTER TABLE `gerechtcategorien`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Code` (`Code`);
+
+--
+-- Indexes for table `gerechtsoorten`
+--
+ALTER TABLE `gerechtsoorten`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Code` (`Code`),
+  ADD KEY `Gerechtcategorie_ID` (`Gerechtcategorie_ID`);
+
+--
+-- Indexes for table `klanten`
+--
+ALTER TABLE `klanten`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Code` (`Code`),
+  ADD KEY `Gerechtsoort_ID` (`Gerechtsoort_ID`);
+
+--
+-- Indexes for table `reserveringen`
+--
+ALTER TABLE `reserveringen`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Datum` (`Datum`),
+  ADD UNIQUE KEY `Tijd` (`Tijd`),
+  ADD KEY `Klant_ID` (`Klant_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bestellingen`
+--
+ALTER TABLE `bestellingen`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gerechtcategorien`
+--
+ALTER TABLE `gerechtcategorien`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gerechtsoorten`
+--
+ALTER TABLE `gerechtsoorten`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `klanten`
+--
+ALTER TABLE `klanten`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reserveringen`
+--
+ALTER TABLE `reserveringen`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bestellingen`
+--
+ALTER TABLE `bestellingen`
+  ADD CONSTRAINT `bestellingen_ibfk_1` FOREIGN KEY (`Reservering_ID`) REFERENCES `reserveringen` (`ID`),
+  ADD CONSTRAINT `bestellingen_ibfk_2` FOREIGN KEY (`Menuitem_ID`) REFERENCES `menuitems` (`ID`);
+
+--
+-- Constraints for table `gerechtsoorten`
+--
+ALTER TABLE `gerechtsoorten`
+  ADD CONSTRAINT `gerechtsoorten_ibfk_1` FOREIGN KEY (`Gerechtcategorie_ID`) REFERENCES `gerechtcategorien` (`ID`);
+
+--
+-- Constraints for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  ADD CONSTRAINT `menuitems_ibfk_1` FOREIGN KEY (`Gerechtsoort_ID`) REFERENCES `gerechtsoorten` (`ID`);
+
+--
+-- Constraints for table `reserveringen`
+--
+ALTER TABLE `reserveringen`
+  ADD CONSTRAINT `reserveringen_ibfk_1` FOREIGN KEY (`Klant_ID`) REFERENCES `klanten` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
